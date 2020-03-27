942f9e29b8a1177bd9bc81a17a705e61b06d9f98
Erick Almeida <erickprofissional@gmail.com>
2020-03-27 00:16:01 -0300

    Cadastro das variaveis Linha e Coluna criado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,29 @@
 package aplicacao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Programa 
 {
 	public static void main(String[] arg) 
 	{
+		Locale.setDefault(Locale.US);
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Matrix [M][N]");
+		
+		System.out.print("Digite o numero de Linhas: ");
+		
+		int l = sc.nextInt();
+		
+		System.out.print("Digite o numero de Colunas: ");
+		int c = sc.nextInt();
+		
+		int matrix[][] = new int[l][c];
+		
+		
+		
+		sc.close();
 		
 	}
 }
