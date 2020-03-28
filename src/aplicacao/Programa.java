7ad7f2a60b09b50be827adc83c40baa4542e66b9
Erick Almeida <erickprofissional@gmail.com>
2020-03-27 21:52:43 -0300

    Concluido Lista posição matriz

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -23,23 +23,56 @@ public class Programa
 		
 		int[][] matriz = new int[l][c];
 		
-		sc.nextLine();
+		System.out.println("Entre com os Valores para Matriz:");
+	
+		System.out.println();
 				
 		for(int i = 0; i < matriz.length; i++) 
 		{
 			for(int j = 0; j < matriz[i].length; j++) 
 			{
-				//System.out.printf("Posicão ["+i+"]"+" ["+j+"] =");
-				
 				matriz[i][j] = sc.nextInt();
 			}
 		}
 		
+		System.out.println();
 		
+		System.out.print("Pesquisar valor Matriz: ");
 		
+		int vMatriz = sc.nextInt();
 		
+		for(int i=0; i<matriz.length; i++ ) 
+		{
+			for(int j=0; j<matriz[i].length; j++)
+			{
+				if(matriz[i][j] == vMatriz)
+				{
+					System.out.println("Posição ["+i+"]["+j+"]");
+						
+					if(j > 0 )
+					{
+						System.out.println("Esquerada: " + matriz[i][j-1]); 
+					}
+					if(i > 0) 
+					{
+						System.out.println("A cima: " + matriz[i-1][j]);
+					}
+					if(j < matriz[i].length-1)
+					{
+						System.out.println("Direita: " + matriz[i][j+1]);
+					}
+					if(i < matriz.length-1) 
+					{
+						System.out.println("Abaixo: " + matriz[i+1][j]);
+					}
+				}
+				/*else 
+				{
+					System.out.println("Valor não encontrato");
+				}*/
+			}
+		}
 		
-		sc.close();
-		
+			sc.close();
 	}
 }
